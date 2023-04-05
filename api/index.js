0e099a0f1efa7752174aa24953f8061e0542d7af
masao.setogawa <ms.job.hunting@gmail.com>
2023-04-05 18:21:10 +0900

    初期コミット

--- api/index.js
+++ api/index.js
@@ -4,8 +4,9 @@ const app = express();
 const PORT = 3001;
 const HOST = "localhost";
 
-app.get("/info", (req, res, next) => {
-  res.send("info APIが呼ばれました");
+app.get("/api/v1/test", (req, res, next) => {
+  console.log("バックエンドのAPIが呼ばれました");
+  res.send("バックエンドのAPIが呼ばれました");
 });
 
 app.listen(PORT, HOST, () => {
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = () => {
+  const rewrites = () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
+      },
+    ];
+  };
+  return {
+    rewrites,
+  };
+};
